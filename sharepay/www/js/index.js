d77396479398694887d7f81af1b614a7eb5f81c4
Pedro Vinicius Alves Diniz <104076903+pedro-vinicius-alves-diniz@users.noreply.github.com>
2024-12-04 12:03:14 +0100

    test di integrazione Firestore

--- sharepay/www/js/index.js
+++ sharepay/www/js/index.js
@@ -1,4 +1,27 @@
-const db = null
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, setDoc } from "../../node_modules/firebase/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC9oyMrynbcxgiCC1OUoz9nHevvxuvIBJc",
+    authDomain: "sharepay-abffa.firebaseapp.com",
+    projectId: "sharepay-abffa",
+    storageBucket: "sharepay-abffa.firebasestorage.app",
+    messagingSenderId: "396784972724",
+    appId: "1:396784972724:web:e66be805ac4174153138e1",
+    measurementId: "G-R3N32Q2NEG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const usersCollection = collection(db, "utenti");
+
 
 const btnAccedere = document.getElementById("btn-accedere");
 const emailInput = document.getElementById('iemail');
@@ -31,9 +54,21 @@ btnAccedere.addEventListener("click", () => {
             passwordInput.classList.remove('animation');
         }, 500)
     } else {
-        console.log('Login realizado com sucesso.')
+        const idUtente = emailInput.value
 
-        window.location.href = 'home.html'
+
+        setDoc(doc(db, "utenti", idUtente), {
+            email: emailInput.value,
+            password: passwordInput.value
+        })
+        .then(() => {
+            console.log('Document written with ID:', idUtente);
+        })
+        .catch(error => {
+            console.error('Error adding document:', error);
+        });
+
+        console.log('Login realizado com sucesso.')
     }
 })
 
